d616b15d5664a96cf04dddfb1163e4a094a6d950
Jailson <jaiusu01@gmail.com>
2023-11-21 20:45:34 -0300

    created TodoList component

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Todo } from "../models/Todo";
+
+const TodoList = () => {
+    const todos: Todo[] = [
+        { id: 1, title: 'Estudar React', done: false },
+        { id: 2, title: 'Estudar C#', done: false }
+    ];
+
+    return(
+        <table className="uk-table">
+            <caption>Lista de Tarefas</caption>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Tarefa</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    todos?.map(
+                        todo => (<div key={todo.id}>{todo.title}</div>)
+                    )
+                }
+            </tbody>
+        </table>
+    );
+}
+
+export default TodoList;
